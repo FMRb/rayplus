7aec9fc979a17b6d639893505eb96b5e9eb0ab52
Felix Rubio <felix@airtame.com>
2018-09-24 22:27:41 +0200

    material on progress

--- hitable.hpp
+++ hitable.hpp
@@ -2,13 +2,18 @@
 
 #include "ray.hpp"
 
-struct hit_record {
+class material;
+
+struct hit_record
+{
   float t;
   vec3 p;
   vec3 normal;
+  material *mat_ptr;
 };
 
-class hitable {
-  public:
-    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+class hitable
+{
+public:
+  virtual bool hit(const ray &r, float t_min, float t_max, hit_record &rec) const = 0;
 };
--- sphere.hpp
+++ sphere.hpp
@@ -1,36 +1,47 @@
 #pragma once
 #include "hitable.hpp"
 
-class sphere: public hitable {
-  public:
-    sphere() {}
-    sphere(vec3 c, float r) { center = c; radius = r; }
-    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
-    vec3 center;
-    float radius;
-
+class sphere : public hitable
+{
+public:
+  sphere() {}
+  sphere(vec3 c, float r, material *m)
+  {
+    center = c;
+    radius = r;
+    mat_ptr = m;
+  }
+  virtual bool hit(const ray &r, float t_min, float t_max, hit_record &rec) const;
+  vec3 center;
+  float radius;
+  material *mat_ptr;
 };
 
-
-bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
-   vec3 oc = r.origin() - center;
+bool sphere::hit(const ray &r, float t_min, float t_max, hit_record &rec) const
+{
+  vec3 oc = r.origin() - center;
   float a = dot(r.direction(), r.direction());
   float b = 2.0 * dot(oc, r.direction());
   float c = dot(oc, oc) - (radius * radius);
   float discriminant = (b * b) - (4 * a * c);
-  if (discriminant > 0) {
-    float temp = (-b - sqrt(discriminant)) / (2.0*a);
-    if (temp < t_max && temp > t_min) {
+  if (discriminant > 0)
+  {
+    float temp = (-b - sqrt(discriminant)) / (2.0 * a);
+    if (temp < t_max && temp > t_min)
+    {
       rec.t = temp;
       rec.p = r.point_at_parameter(rec.t);
       rec.normal = (rec.p - center) / radius;
+      rec.mat_ptr = mat_ptr;
       return true;
     }
-    temp = (-b + sqrt(discriminant)) / (2.0*a);
-    if (temp < t_max && temp > t_min) {
+    temp = (-b + sqrt(discriminant)) / (2.0 * a);
+    if (temp < t_max && temp > t_min)
+    {
       rec.t = temp;
       rec.p = r.point_at_parameter(rec.t);
       rec.normal = (rec.p - center) / radius;
+      rec.mat_ptr = mat_ptr;
       return true;
     }
   }
